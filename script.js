cf32f2f2fd4a47e4a3074f3e557ed5c9e727fcf9
SyrianTiger74 <maher.abou.asla@gmail.com>
2023-10-02 22:35:00 +0300

    task8-12-loops

---
+++ script.js
@@ -0,0 +1,54 @@
+function task8() {
+    for (let i = 100; i >= 1; i--) {
+      // console.log(i * 5)
+      let num = 5
+      let result = i * num
+      console.log(`${i} * ${num} = ${result}`)
+    }
+  }
+  
+  task8()
+  
+  function task9() {
+    for (let i = 100; i >= 1; i--) {
+      // console.log(i + 5)
+      let num = 5
+      let result = i + num
+      console.log(`${i} + ${num} = ${result}`)
+    }
+  }
+  
+  // task9()
+  
+  function task10() {
+    for (let i = 100; i >= 1; i--) {
+      // console.log(i - 2)
+      let num = 2
+      let result = i - num
+      console.log(`${i} - ${num} = ${result}`)
+    }
+  }
+  
+  // task10()
+  
+  function task11() {
+    for (let i = 100; i >= 1; i--) {
+      // console.log(Math.pow(i, 2))
+      let num = 2
+      let result = Math.pow(i, num)
+      console.log(`${i}\u00B2 = ${result}`)
+    }
+  }
+  
+//   task11()
+  
+  function task12() {
+    for (let i = 100; i >= 1; i--) {
+      // console.log(Math.pow(i, 3))
+      let num = 3
+      let result = Math.pow(i, num)
+      console.log(`${i}\u00B3 = ${result}`)
+    }
+  }
+  
+//   task12()
